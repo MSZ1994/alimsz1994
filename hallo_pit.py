e5139b90aa9dff122bcfb76a038a435241c87e94
Alitkn2020 <139393508+MSZ1994@users.noreply.github.com>
2026-06-29 12:59:06 +0330

    Update hallo_pit.py

--- hallo_pit.py
+++ hallo_pit.py
@@ -21,4 +21,4 @@ if confirm == "yes":
     print("Contract signed successfully.")
 else:
     print("Signing cancelled.")
-# here we are guys welcome new  :0
+# here we are guys welcome new boys  :0
